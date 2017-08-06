05f95cc4ab2851618d993947a463920abea650d1
sparrow <sparrow@SparrowdeMacBook-Pro.local>
2017-08-06 18:22:04 +0800

    Finish initial completed version
    Add DownloadContent to download content to local.
    Add CallbackAPI_URL for wechat.
    
    Function content_type() is judging the content type of video, single
    image or a set of image, return defined string accordingly.
    
    Function findVideoUrl() is to locate the video url, return url string.
    
    Function findImageUrl_Single() is to locate the single image url,
    return url string.
    
    Function findImageUrl_Set() is to locate serious url of images, return
    urls list.
    
    Function getPostname() is to get post postfix to name the content.
    
    Function DownloadVideo(), DownloadImage_Single(),  DownloadImage_set(),
    are downloading content to local.
    
    Function progress_report() is mine defined reporthook in
    urllib.request.urlretrieve(), showing progress of downloading.

--- Instgram_Crawler.py
+++ Instgram_Crawler.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
   Author:  Sparrow
-  Purpose: downloading one entire blog from Tumblr once.
-  Created: 2017-1.1
+  Purpose: Crawl the single post of content from Instagram application
+  Created: 2017-08-06
 """
 import re
 import urllib.request
@@ -11,46 +11,129 @@ import traceback
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 
+Video = 'GraphVideo'
+Image_single = 'GraphImage'
+Image_set = 'GraphSidecar'
+
 def getHtml(url):
     url = quote(url, safe="/:?=")
     try:
         page = urllib.request.urlopen(url)
-        html = page.read()
+        html = page.read().decode('utf-8')
         # print(html)
         return html
     except:
         traceback.print_exc()
-        print('The URL you requested could not be found in Module image')
+        print('The URL you requested could not be found.')
         return 'Html'
 
-def findImageUrl(url):
-    html = getHtml(url)
-    Soup = BeautifulSoup(html, 'lxml')
-    tag = Soup.select('#react-root > section > main > div > div > article > div._h5v2a._kqf30 > div > div > div._jjzlb > img')
+def content_type(HTML):
+    html = HTML
+    isVideore_exp = r'"is_video": true'
+    isVideore = re.compile(isVideore_exp)
+    isVideotag = re.findall(isVideore, html)
 
-    if tag[0]:
-        imageUrl = tag[0].get('src')
-        # print(imageUrl)
-        # print(tag)
+    if isVideotag:
+        print("Content is video!")
+        typename = Video
+
+    else:
+        isImageSinglere_exp = r'\{"PostPage": \[\{"graphql": \{"shortcode_media": \{"__typename": "GraphImage"'
+        isImageSinglere = re.compile(isImageSinglere_exp)
+        isImageSingle = re.findall(isImageSinglere, HTML)
+
+        if isImageSingle:
+            print("Content is just a single Image!")
+            typename = Image_single
+        else:
+            print("Contents are a set of Images!")
+            typename = Image_set
+
+    return typename
+
+
+def findVideoUrl(HTML):
+    html = HTML
+    videore_raw = r'"video_url": "(https://scontent-sin6-2\.cdninstagram\.com/t50\.2886-16/.*_n\.mp4)",'
+    videore = re.compile(videore_raw)
+    videotag = re.findall(videore, html)
+    # print(videotag)
+
+    if videotag:
+        videoUrl = videotag[0]
+        print(videoUrl)
+        return videoUrl
+    else:
+        print('The Video url seems disappear...')
+        return False
+
+
+def findImageUrl_Single(HTML):
+    html = HTML
+    displayre = r'"display_url": "(https://scontent-sin6-2\.cdninstagram\.com/t51\.2885-15/e35/.*_n\.jpg)", "display_resources": \[\], "is_video": false,'
+    imgurltagre = re.compile(displayre)
+    imageurltag = re.findall(imgurltagre, html)
+    # print(displayre,imageurltag)
+
+    if imageurltag:
+        imageUrl = imageurltag[0]
+        print(imageUrl)
         return imageUrl
     else:
         print('The image url seems disappear...')
         return False
+
+def findImageUrl_Set(HTML):
+    html = HTML
+    displayre = r'"display_url": "(https://.*_n\.jpg)", "display_resources": \[\], "is_video": false,'
+    imgurllistre = re.compile(displayre)
+    imageurllist_raw = re.findall(imgurllistre, html)
+    if imageurllist_raw:
+        imglistre_exp = r'"display_url": "(https://scontent-sin6-2\.cdninstagram\.com/t51\.2885-15/e35/\d*_\d*_\d*_n\.jpg)'
+        imglistre = re.compile(imglistre_exp)
+        imageurllists = re.findall(imglistre, imageurllist_raw[0])
+        # print(imglistre, imageurllist_raw[0], '\n\n', imageurllists)
+
+        if imageurllists :
+            imageurllist = list(set(imageurllists))
+            print(imageurllist)
+            return imageurllist
+        else:
+            print('The set of image urls seems disappear...')
+            return False
+
 
 def getPostname(posturl):
     reg = r'https://www.instagram.com/p/(.*)/'
     postnameRe = re.compile(reg)
     postnames = re.findall(postnameRe, posturl)
     if postnames:
-        print(postnames[0])
+        # print(postnames[0])
         return postnames[0]
     else:
         print('Lost postname...')
         return False
 
-def DownloadImage(url):
-    ImgUrl = findImageUrl(url)
-    ImgName = getPostname(url)
+
+def DownloadVideo(videourl, postname):
+    VideoUrl = videourl
+    VideoName = postname
+
+    if VideoUrl and VideoName:
+        path = 'InstgramVideo/'
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        target = path + '%s.mp4' % VideoName
+        print("Downloading Video %s \n" % target)
+        try:
+            urllib.request.urlretrieve(VideoUrl, target, progress_report)
+        except:
+            print("The Video seems disappear...")
+
+def DownloadImage_Single(imageurl, postname):
+    ImgUrl = imageurl
+    ImgName = postname
 
     if ImgUrl and ImgName:
         path = 'InstgramImage/'
@@ -60,11 +143,107 @@ def DownloadImage(url):
         target = path + '%s.jpg' % ImgName
         print("Downloading %s \n" % target)
         try:
-            urllib.request.urlretrieve(ImgUrl, target)
+            urllib.request.urlretrieve(ImgUrl, target, progress_report)
         except:
-            print("The image seems disappear...")
+            print("The image seems could not be downloaded...")
+
+def DownloadImage_set(imageurllist, postname):
+    ImageUrlList = imageurllist
+    PostName = postname
+
+    if ImageUrlList and PostName:
+        path = 'InstgramImage/'
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        count = 0
+        for imageurl in ImageUrlList:
+            count += 1
+            target = path + '%s_%d.jpg' % (PostName, count)
+            print("Downloading %s \n" % target)
+            try:
+                urllib.request.urlretrieve(imageurl, target, progress_report)
+            except:
+                print("The %s image seems could not be downloaded..." % count)
+
+
+def progress_report(blocknum, blocksize, totalsize):
+    #blocknum: 已经下载的数据块; blocksize: 数据块的大小; totalsize: 远程文件的大小
+    percent = 100.0 * blocknum * blocksize / totalsize
+    if percent >= 100:
+        percent = 100.0
+    print("Downloading block %d, %d/%d, ===> %.2f%%" % (blocknum, blocknum*blocksize, totalsize, percent))
+
+def DownloadContent(url):
+    HTML = getHtml(url)
+    ContentType = content_type(HTML)
+
+    if ContentType == Video:
+        VideoUrl = findVideoUrl(HTML)
+        VideoName = getPostname(url)
+        DownloadVideo(VideoUrl, VideoName)
+        return VideoUrl
+    elif ContentType == Image_single:
+        ImageUrl = findImageUrl_Single(HTML)
+        ImageName = getPostname(url)
+        DownloadImage_Single(ImageUrl, ImageName)
+        return ImageUrl
+    else:
+        ImageUrlList = findImageUrl_Set(HTML)
+        PostName = getPostname(url)
+        DownloadImage_set(ImageUrlList, PostName)
+        return ImageUrlList
+
+def CallbackAPI_URL(url):
+    HTML = getHtml(url)
+    ContentType = content_type(HTML)
+
+    if ContentType == Video:
+        VideoUrl = findVideoUrl(HTML)
+        return VideoUrl
+
+    elif ContentType == Image_single:
+        ImageUrl = findImageUrl_Single(HTML)
+        return ImageUrl
+    else:
+        ImageUrlList = findImageUrl_Set(HTML)
+        return ImageUrlList
+
+
+def main():
+    print('''
+                    ---------------------------------
+                       Welcome to Instgram Crawler!
+                    ---------------------------------
+                    Author:  Sparrow
+                    Purpose: downloading images and videos from any Tumblr once.
+                    Created: 2017.08.06
+                    Version: 1.6
+                    Manual: https://github.com/sparrow629/Instagram_Crawler
+                    署名-非商业使用-禁止演绎 (by-nc-nd)：
+                    ''')
+    select = 'N'
+    post_reg = r'https://www.instagram.com/p/.*?'
+    while not (select == 'Y'):
+
+        URL = input('Input url: ')
+        if re.match(post_reg, URL):
+            DownloadContent(URL)
+        else:
+            print("The url seems not to be a post... Please input again!")
+
+        select = input("Do you want to Quit? [Y/N]")
 
 if __name__ == '__main__':
-    url = 'https://www.instagram.com/p/BW6lCSchA94/'
+    main()
 
-    DownloadImage(url)
+    # url1_multi = 'https://www.instagram.com/p/BXZyl4GBhXy/'
+    # url_video = 'https://www.instagram.com/p/BXMc-NXhwaL/'
+    # url_video1 = 'https://www.instagram.com/p/BXPI0yfAHBz/?taken-by=xiamei0828'
+    # url2_single = 'https://www.instagram.com/p/BXbuLwlFL7Q/'
+    # url3_single = 'https://www.instagram.com/p/BXUSWaYgc_y/'
+    # url4_multi = 'https://www.instagram.com/p/BXaI4Dkhhv5/'
+    # url5_multi = 'https://www.instagram.com/p/BW8-SvFBqge/?taken-by=ssovely1024'
+    
+    # ContentUrl = CallbackAPI_URL(url1_multi)
+    # print(ContentUrl)
